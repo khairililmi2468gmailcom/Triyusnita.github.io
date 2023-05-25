9cf0043c2ea437d3ac97b6f8800e3f988d51abef
khairilGayo <khairililmi2468@gmail.com>
2023-05-25 22:22:16 +0700

    hosting github

---
+++ talwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    content: ['index.html'],
+    darkMode: 'class',
+    theme: {
+        container: {
+            center: true,
+            padding: '16px',
+        },
+        extend: {
+            colors: {
+                primary: '#6a0a63',
+                secondary: '#6a0a63',
+                dark: '#6a0a63',
+            },
+            screens: {
+                '2xl': '1320px',
+            },
+        },
+    },
+    plugins: [],
+};
